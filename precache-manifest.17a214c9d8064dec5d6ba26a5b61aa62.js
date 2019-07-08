9988ffbe10af71e6bd07cb1fb991492b15eb08ea
Sharika.K <Sharika.Krishnaprasad@envestnetrs.com>
2019-07-08 14:42:53 +0530

    react demo

---
+++ precache-manifest.17a214c9d8064dec5d6ba26a5b61aa62.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73826c0dafe840c2ddd682fbaa4e3c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d16c693692d3ea55ebf",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "abc7cd88f1be4d4a423c",
+    "url": "/static/css/main.ab9f0b4e.chunk.css"
+  },
+  {
+    "revision": "7d16c693692d3ea55ebf",
+    "url": "/static/js/2.061697b4.chunk.js"
+  },
+  {
+    "revision": "abc7cd88f1be4d4a423c",
+    "url": "/static/js/main.6c6f4ccd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e5b3bafed557ec5dab36f35c4e8db2f",
+    "url": "/static/media/angel.6e5b3baf.PNG"
+  },
+  {
+    "revision": "9275df0f30f66d90774acf9946a3eb8b",
+    "url": "/static/media/carousal1.9275df0f.jpg"
+  },
+  {
+    "revision": "2ddbc9725a0ef37c29697f3ecd89f229",
+    "url": "/static/media/carousal2.2ddbc972.png"
+  },
+  {
+    "revision": "03b4dea49ab5f8413fa107ef41e87042",
+    "url": "/static/media/carousal3.03b4dea4.jpg"
+  },
+  {
+    "revision": "83476ee834eadf56c06aa968e253360e",
+    "url": "/static/media/doctor1.83476ee8.png"
+  },
+  {
+    "revision": "ae33102780410039af763a94197f4ede",
+    "url": "/static/media/doctor2.ae331027.png"
+  },
+  {
+    "revision": "bfc7584cf029ed238134c92e706e6106",
+    "url": "/static/media/gene.bfc7584c.PNG"
+  },
+  {
+    "revision": "a6c2a323f821c1cfe963670f00c96dd6",
+    "url": "/static/media/logo1.a6c2a323.png"
+  },
+  {
+    "revision": "de38c1ab8c063f3ee9f1d35a50251086",
+    "url": "/static/media/messageIcon.de38c1ab.PNG"
+  },
+  {
+    "revision": "deadd1e26fc33678469e1023de3692be",
+    "url": "/static/media/racheal.deadd1e2.PNG"
+  },
+  {
+    "revision": "fe6d698e7d87dcba9beec36a047ab899",
+    "url": "/static/media/starIcon.fe6d698e.PNG"
+  },
+  {
+    "revision": "d2f321b1586623c45e40df905cab1956",
+    "url": "/static/media/todd.d2f321b1.PNG"
+  }
+]);
